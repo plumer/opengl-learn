b41bbb08405d7ee7f0713218acb6d2a735e90906
Vocryan Koverald <yzx19942013@gmail.com>
2015-01-10 12:17:09 +0800

    a new(old) example used

---
+++ 01-old/02-hello/hello.c
@@ -0,0 +1,49 @@
+#include <GL/gl.h>
+#include <GL/glut.h>
+
+void display() {
+	// clear all pixels
+	glClear(GL_COLOR_BUFFER_BIT);
+	// draw white polygon
+	glColor3f(1.0, 1.0, 1.0);
+	glBegin(GL_POLYGON);
+		glVertex3f(0.25, 0.25, 0.0);
+		glVertex3f(0.75, 0.25, 0.0);
+		glVertex3f(0.75, 0.75, 0.0);
+		glVertex3f(0.25, 0.75, 0.0);
+	glEnd();
+
+	// don't wait! start processing buffered OpenGl routines
+	glFlush();
+}
+
+void init() {
+	// select clearing (background) color
+	glClearColor(0.0,0.0,0.0,0.0);
+
+	// initialize viewing values
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+}
+
+// Declare initial window size, position, and display mode
+// Open window with "hello" in its title bar.
+// Call initialization routines.
+// Register callback function to display graphics.
+// Enter main loop and process events.
+
+int main(int argc, char **argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(250, 250);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow(argv[0]);
+
+	init();
+
+	glutDisplayFunc(display);
+	glutMainLoop();
+
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-opengl-learn
-============
+# opengl-learn
 
 learning OpenGL
+
+## 2015.1.10:
+
+Tested the very first example in OpenGL Redbook (**8th Edition**) but failed:
+> Segmentation fault (core dumped)
+
+Find another GL tutorial on [GLProgramming](http://www.glprogramming.com/red/index.html),
+
+examples are simpler and easier to understand.
+
+Maybe that's an earlier version of Redbook.
